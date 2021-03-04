718e9538d2e403fc798c488c6b238a3d92442d10
MinY-Lee <leemy08@naver.com>
2021-03-04 15:04:58 +0000

    Challenge 01

---
+++ python_basic/challenge01.py
@@ -0,0 +1,30 @@
+def plus(a, b):
+    return (int(a) + int(b))
+
+
+def minus(a, b):
+    return (int(a) - int(b))
+
+
+def times(a, b):
+    return (int(a) * int(b))
+
+
+def division(a, b):
+    return (int(a) / int(b))
+
+
+def negation(a):
+    return (int(a) * -1)
+
+
+def power(a, b):
+    return (int(a)**int(b))
+
+
+print(plus(1, 2))
+print(minus(4, 3))
+print(times(5, 6))
+print(division(8, 4))
+print(negation(7))
+print(power(2, 10))
