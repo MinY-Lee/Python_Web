bbf19b64e45bfbfa9e0a4d3bfe41c907a5e1b313
MinY-Lee <leemy08@naver.com>
2021-03-08 06:57:37 +0000

    for

--- python_basic/class01.py
+++ python_basic/class01.py
@@ -1,13 +1,18 @@
-def age_check(age):
-  print(f"your are {age}")
-  if age<18:
-    print("you can't drink")
-  elif age==18:
-    print("you are new to this!")
-  elif age>20 and age<25:
-    print("you are still kind of young")
-  else:
-    print("enjoy your drink")
+# def age_check(age):
+#   print(f"your are {age}")
+#   if age<18:
+#     print("you can't drink")
+#   elif age==18:
+#     print("you are new to this!")
+#   elif age>20 and age<25:
+#     print("you are still kind of young")
+#   else:
+#     print("enjoy your drink")
 
 
-age_check(29)
+# age_check(29)
+
+days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+
+for day in days:
+  print (day)
